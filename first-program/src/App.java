8a10d8f5f9b84a612cad49f791542bdcf0f3368e
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2026-03-05 15:36:45 -0300

    praticando saída de dados no terminal

--- first-program/src/App.java
+++ first-program/src/App.java
@@ -1,5 +1,38 @@
+import java.text.DecimalFormat;
+
 public class App{
     public static void main(String[] args){
-        System.out.println("Olá mundo!");
+        // exercício de print
+        System.out.println("Products:");
+
+        String computer = "Computer";
+        double computerPrice = 2100.0;
+
+        DecimalFormat decimalPriceFormat = new DecimalFormat("0.00");
+        System.out.println(computer + ", which price is $ " + decimalPriceFormat.format(computerPrice));
+    
+        String officeDesk = "Office desk";
+        double officeDeskPrice = 650.50;
+
+        System.out.println(officeDesk + ", wich price is $ " + decimalPriceFormat.format(officeDeskPrice) + "\n");
+        
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+
+        System.out.println("Record: " + age + " years old, " + "code " + code + " and gender: " + gender + "\n");
+
+        double measure = 53.234567;
+        DecimalFormat measueDecimalFormat = new DecimalFormat("0.00000000");
+
+        System.out.println("Measue with eight decimal places: " + measueDecimalFormat.format(measure));
+        
+        DecimalFormat roudedDecimalFormat = new DecimalFormat("0.000");
+        System.out.println("Rouded (three decimal places): " + roudedDecimalFormat.format(measure));
+    
+        double usFormat = 53.235   ;
+
+        System.out.println("US decimal point: " + usFormat);
+        
     }
 }
