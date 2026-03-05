66535c11df518efbf2b22f8c812c44852801ce76
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2026-03-05 16:55:46 -0300

    aprendendo sobre entrada de dados com Java

---
+++ first-program/src/EntradaDeDados.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class EntradaDeDados{
+    public static void main(String[] args){
+        // o scanner é utilizado para a entrada de dados no Java
+        Scanner sc = new Scanner(System.in);
+
+        // lê string
+        String x = sc.next();
+        System.out.println("string is: " + x);
+
+        // lê número inteiro
+        int myInt = sc.nextInt();
+        System.out.println("int is: " + myInt);
+
+        // lê número flutuante
+        double myDouble = sc.nextDouble();
+        System.out.println("double is: " + myDouble);
+
+        // lê char
+        char y = sc.next().charAt(0);
+        System.out.println("my char is: " + y);
+
+        sc.close();
+    }
+}
