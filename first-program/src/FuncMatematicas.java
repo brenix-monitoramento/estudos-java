5685e4599e836876d064750a3379db7800fdbc74
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2026-03-07 20:38:16 -0300

    aprendendo sobre funções matemáticas

---
+++ first-program/src/FuncMatematicas.java
@@ -0,0 +1,18 @@
+public class FuncMatematicas{
+    public static void main(String[] args){
+        // Funções matemáticas
+
+        // Math.sqrt(x) calcular raíz quadrada de um valor.
+        double raiz = Math.sqrt(100);
+        System.out.println("raíz de 100 é igual a: " + raiz);
+
+        // Math.pow(x, y) calcular o valor de x elevado a y.
+        double potencia = Math.pow(2, 4);
+        System.out.println("2 elevado a 4 é igual a: " + potencia);
+
+        // Math.abs(x) calcular o valor absoluto de x.
+        double absoluto = Math.abs(23);
+        System.out.println("absoluto de 23 é igual a: " + absoluto); 
+    }
+}
+
