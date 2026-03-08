973882e395b239dc70e4077f235a614a6c66c9a8
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2026-03-07 21:36:45 -0300

    operadores lógicos

---
+++ first-program/src/Logicos.java
@@ -0,0 +1,8 @@
+public class Logicos{
+    public static void main(String[] args){
+        // operadores lógicos
+        // &&: significa E
+        // ||: significa OU
+        // !: significa NÃO (inversão de valor)
+    }
+}
