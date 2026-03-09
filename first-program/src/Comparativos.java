fa54831c790fe820a2923064a5a70cb91d33cd1a
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2026-03-09 11:27:05 -0300

    alterações não salvas

---
+++ first-program/src/Comparativos.java
@@ -0,0 +1,11 @@
+public class Comparativos{
+    public static void main(String[] args){
+        // Operadores comparativos
+        // > maior
+        // < menor
+        // >= maior ou igual
+        // <= menor ou igual
+        // == igual
+        // != diferente
+    }
+}
