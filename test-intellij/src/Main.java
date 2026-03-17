8b652035778a098aad73afa33d1c99d6efa82f79
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2026-03-17 11:41:21 -0300

    segundo commit no intellij.

--- test-intellij/src/Main.java
+++ test-intellij/src/Main.java
@@ -1,5 +1,15 @@
 public class Main{
     public static void main(String[] args){
         System.out.println("Olá mundo!");
+
+        bool();
+    }
+
+    public static void bool(){
+        int x = 23;
+        double y = 45.67;
+
+        String output = (y < x) ? "Olá" : "Mundo";
+        System.out.println("Result " + output);
     }
 }
